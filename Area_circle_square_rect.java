c3dd16473a6a776852e51fc99bfef419d651c459
Amruta <amruta4kulkarni@gmail.com>
2025-07-02 12:30:29 +0530

    Initial Commit

---
+++ Area_circle_square_rect.java
@@ -0,0 +1,25 @@
+package assignmentmdk;
+
+import java.util.Scanner;
+
+public class Area_circle_square_rect {
+
+	
+	public static void main(String[] args) {
+		
+
+		Double areaOfCircle;
+		Scanner s1=new Scanner(System.in);
+		
+		System.out.print("Enter radious of circle");
+	/*	double radious=scanner.nextDouble();
+		double radius = scanner.nextDouble();
+		
+		areaOfCircle=3.14*radious*radious;	
+		System.out.println("Area of the circle = " + areaOfCircle);
+		
+		scanner.close();
+*/
+	}
+
+}
